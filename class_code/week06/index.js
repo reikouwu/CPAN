9b7ca7e57f64495d1a9d3a6e8aea33dec1e7e8ed
Reiko <nagitookomaeda100@gmail.com>
2025-03-18 14:45:38 -0400

    more code

--- class_code/week06/index.js
+++ class_code/week06/index.js
@@ -3,10 +3,15 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import book_router from './routers/book_router.js';
+import user_router from './routers/user_router.js';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
@@ -17,3 +22,5 @@ mongoose.connect(process.env.MONGO_URI)
 })
 
 app.use(cors("/book", book_router));
+
+app.use(cors("/user", user_router));
